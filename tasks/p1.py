396edae986a081ac539c2335efc8bd05e7e479af
nbobrov8 <bobrovn8@yandex.ru>
2021-10-31 22:59:41 +0300

    added tasks and doc

---
+++ tasks/p1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def fun1(a):
+    x = a * 3
+
+    def fun2(b):
+        nonlocal x
+        return b + x
+    return fun2
+
+
+test_fun = fun1(4)
+print(test_fun(7))
